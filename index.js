7289053b8c01a44a025d54be99a99e3438df552e
Rob Cochran <rob.cochran@bbc.co.uk>
2018-06-28 14:28:55 +0100

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var responses = require('./responses')
 
 
 app.get('/', function(req, res) {
@@ -8,8 +9,29 @@ app.get('/', function(req, res) {
 });
 
 app.use(express.static('client'))
-app.use('/sounds', express.static('sounds'))
+
+app.get('/ballme/:id', function(req, res) {
+  var val = psuedoMaths(req.params.id)
+  var resp = ''
+  if (val > 0 && val < 0.3) {
+    resp = responses.negative[Math.floor(Math.random() * responses.negative.length)]
+  } else if ( val > 0.3 && val < 0.6) {
+    resp = responses.neutral[Math.floor(Math.random() * responses.neutral.length)]
+  } else {
+    resp = responses.positive[Math.floor(Math.random() * responses.positive.length)]
+  }
+
+  res.send({result: resp}, 200);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`app listening on localhost:${process.env.PORT || 8080}`)
 });
+
+function psuedoMaths(id) {
+  var kludge = String(Number(id) / 3.14)
+  var pC = kludge.substring(kludge.length - 2,kludge.length) / 100
+  return pC
+}
+
+//https://js.tensorflow.org/#getting-started
---
+++ responses.js
@@ -0,0 +1,30 @@
+var responses = {
+  positive: [
+    { text: "It is certain"},
+    { text: "It is decidedly so"},
+    { text: "Without a doubt"},
+    { text: "Yes - definitely"},
+    { text: "You may rely on it"},
+    { text: "As I see it, yes"},
+    { text: "Most likely"},
+    { text: "Outlook good"},
+    { text: "Yes"},
+    { text: "Signs point to yes"},
+  ],
+  neutral: [
+    { text: "Reply hazy, try again"},
+    { text: "Ask again later"},
+    { text: "Without a doubt"},
+    { text: "Better not tell you now"},
+    { text: "Concentrate and ask again"},
+  ],
+  negative: [
+    { text: "Don't count on it"},
+    { text: "My reply is no"},
+    { text: "My sources say no"},
+    { text: "Outlook not so good"},
+    { text: "Very doubtful"},
+  ]
+}
+
+module.exports = responses
